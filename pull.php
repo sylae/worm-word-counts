bee4fe0f5e5eb5448e4b90f1357faba6000d9c4f
Keira Dueck <sylae@calref.net>
2020-05-03 01:13:35 -0700

    pull ward as well

--- pull.php
+++ pull.php
@@ -2,12 +2,30 @@
 
 require 'vendor/autoload.php';
 
+if (!in_array($argv[1] ?? "worm", ['worm', 'ward'])) {
+	die("what fic is that?");
+}
+$fic = $argv[1] ?? "worm";
+
+switch ($fic) {
+	case "worm":
+		$baseURL = 'https://parahumans.wordpress.com/table-of-contents/';
+		$class = "#post-2404 > .entry-content";
+		$folder = "data-worm";
+		break;
+	case "ward":
+		$baseURL = 'https://www.parahumans.net/table-of-contents/';
+		$class = "#post-48 > .entry-content";
+		$folder = "data-ward";
+		break;
+}
+
 $client = new \GuzzleHttp\Client();
-$res = $client->request('GET', 'https://parahumans.wordpress.com/table-of-contents/');
+$res = $client->request('GET', $baseURL);
 $b = $res->getBody();
-$qp = html5qp((string) $b, "#post-2404 > .entry-content");
+$qp = html5qp((string) $b, $class);
 $urls = [];
-html5qp($qp, "a")->each(function($index, $item) use (&$urls) {
+html5qp($qp, "a")->each(function($index, $item) use (&$urls, $fic) {
 	$url = html5qp($item)->attr("href");
 	if ($url[0] != "h") {
 		$url = "https://$url";
@@ -15,7 +33,11 @@ html5qp($qp, "a")->each(function($index, $item) use (&$urls) {
 	if (strstr($url, "?share=")) {
 		return;
 	}
+	if ($fic == "ward" && stripos($url, "glow-worm") !== false) {
+		return;
+	}
 	$urls[] = $url;
+	
 	if ($url == "https://parahumans.wordpress.com/2013/11/02/teneral-e-1/") { // wildbow pls
 		$urls[] = "https://parahumans.wordpress.com/2013/11/05/teneral-e-2/";
 	}
@@ -27,6 +49,6 @@ foreach($urls as $id=>$url) {
 	$chapter = $client->request('GET', $url)->getBody();
 	
 	$text = html5qp((string) $chapter, ".entry-content")->text();
-	file_put_contents("data/$id.txt", $text);
+	file_put_contents("$folder/$id.txt", $text);
 	sleep(1);
 }
